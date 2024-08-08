e8e1a4946356b9e68a733c3cd2e5fcae0c5c1108
Vincenzo675 <vinceth349@gmail.com>
2024-08-08 20:09:11 +0530

    Update sriparna.py

--- sriparna/sriparna.py
+++ sriparna/sriparna.py
@@ -1,10 +1,10 @@
-# Copyright (c) 2024, Ayus Chatterjee
+# Copyright (c) 2024, Vince Thongam
 # All rights reserved.
 
 # This source code is licensed under the GPL-3.0 license found in the
 # LICENSE file in the root directory of this source tree.
 # If you don't have the source tree alternately this file can be found at
-# <https://github.com/ayusc/termux-sriparna>
+# <https://github.com/Vincenzo675/termux-sriparna>
 
 import os
 import re
