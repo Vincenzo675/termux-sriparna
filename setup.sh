fd200cd630524d4f08e9d8dc47172d7d57e22593
Vincenzo675 <vinceth349@gmail.com>
2024-08-08 20:14:59 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Copyright (c) 2024, Ayus Chatterjee
+# Copyright (c) 2024, Vince Thongam 
 # All rights reserved.
 
 # This source code is licensed under the GPL-3.0 license found in the
 # LICENSE file in the root directory of this source tree.
-# If you don't have the source tree alternately this file can be found at <https://github.com/ayusc/termux-sriparna>
+# If you don't have the source tree alternately this file can be found at <https://github.com/Vincenzo675/termux-sriparna>
 
 yes no | pkg update -y 
 yes no | pkg upgrade -y 
